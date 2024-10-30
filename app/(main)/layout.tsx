afc9e4daaee85974cca778bade3afcae44b99a01
Mariana Mena <maranashokry@ahoo.com>
2024-10-30 13:53:58 -0500

    fix chart bug

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,15 @@
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      {' '}
       <Navbar />
-      <div className="flex">
-        <div className="hidden md:block h-[100vh] ">
+      <div className='flex'>
+        <div className='hidden md:block h-[100vh] w-[300px]'>
           <Sidebar />
         </div>
-        <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
+        <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
       </div>
     </>
   );
