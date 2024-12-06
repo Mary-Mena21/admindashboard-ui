a56139dbfbfa9d3b1e167f07c1917b70308967fc
Mariana Mena <maranashokry@ahoo.com>
2024-12-05 19:47:37 -0600

    comment edit link

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -51,11 +51,11 @@ const PostsTable = ({ limit, title }: PostsTableProps) => {
                 {post.date}
               </TableCell>
               <TableCell>
-                <Link href={`/posts/edit/${post.id}`}>
+                {/* <Link href={`/posts/edit/${post.id}`}>
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs">
                     Edit
                   </button>
-                </Link>
+                </Link> */}
               </TableCell>
             </TableRow>
           ))}
